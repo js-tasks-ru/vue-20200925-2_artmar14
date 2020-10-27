6e9712239fa994560213bfd55a9b679585378fc2
Artem <artmar14@gmail.com>
2020-10-27 12:29:44 +0200

    add solution for 04-SPA 04-Layouts

--- 04-SPA/04-Layouts/components/BaseLayout.vue
+++ 04-SPA/04-Layouts/components/BaseLayout.vue
@@ -1,8 +1,24 @@
-<template></template>
+<template>
+  <div class="wrapper bg-grey">
+    <TheHeader />
+    <main class="main">
+      <router-view />
+    </main>
+    <TheFooter />
+  </div>
+</template>
 
 <script>
+import TheHeader from './TheHeader';
+import TheFooter from './TheFooter';
+
 export default {
   name: 'BaseLayout',
+
+  components: {
+    TheHeader,
+    TheFooter,
+  },
 };
 </script>
 

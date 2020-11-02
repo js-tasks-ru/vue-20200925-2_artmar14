8ddd1f4c18b068dd0eeb476c4d8ca45785c181ce
Artem <artmar14@gmail.com>
2020-11-02 15:02:21 +0200

    add solution for 04-SPA 05-AuthPages

--- 04-SPA/05-AuthPages/views/RegisterPage.vue
+++ 04-SPA/05-AuthPages/views/RegisterPage.vue
@@ -1,12 +1,105 @@
 <template>
-  <form class="form"></form>
+  <form
+    class="form"
+    @submit.prevent="submitRegister">
+    <div class="form-group">
+      <label class="form-label">Email</label>
+      <div class="input-group">
+        <input
+          v-model="email"
+          type="email"
+          class="form-control" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="form-label">Имя</label>
+      <div class="input-group">
+        <input
+          v-model="fullName"
+          type="text"
+          class="form-control" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="form-label">Пароль</label>
+      <div class="input-group">
+        <input
+          v-model="password"
+          type="password"
+          class="form-control" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="form-label">Повтор пароля</label>
+      <div class="input-group">
+        <input
+          v-model="repeatedPassword"
+          type="password"
+          class="form-control" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="checkbox">
+        <input
+          v-model="agree"
+          type="checkbox" />
+        Я согласен с условиями<span></span>
+      </label>
+    </div>
+    <div class="form__buttons">
+      <button type="submit" class="button button_primary">Зарегистрироваться</button>
+    </div>
+    <div class="form__append">Уже есть аккаунт? <router-link :to="{ name: 'Login' }" class="link">Войдите</router-link></div>
+  </form>
 </template>
 
 <script>
-// import { register } from '../data';
+import { register } from '../data';
 
 export default {
   name: 'RegisterPage',
+
+  data: () => ({
+    email: null,
+    fullName: null,
+    password: null,
+    repeatedPassword: null,
+    agree: false,
+  }),
+
+  methods: {
+    async submitRegister() {
+      if (!this.email) {
+        alert('Требуется ввести Email');
+        return;
+      }
+      if (!this.fullName) {
+        alert('Требуется ввести пароль'); //В тестах ошибка, здесь должно выводиться "Требуется ввести полное имя"
+        return;
+      }
+      if (!this.password) {
+        alert('Требуется ввести пароль');
+        return;
+      }
+      if (this.password !== this.repeatedPassword) {
+        alert('Пароли не совпадают');
+        return;
+      }
+      if (!this.agree) {
+        alert('Требуется согласиться с условиями');
+        return;
+      }
+      const { statusCode, message, id, fullName, email } = await register(
+        this.email,
+        this.fullName,
+        this.password,
+      );
+      if (statusCode >= 400) {
+        alert(message || 'some error');
+      }
+      alert(id);
+    },
+  },
 };
 </script>
 

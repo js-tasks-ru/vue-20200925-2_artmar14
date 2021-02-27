0db747d074dee870ac2117516b2f52fb61ebbe5d
Artem <artmar14@gmail.com>
2021-02-27 13:35:53 +0200

    solution for AppPagination task

--- 08-interactions-and-logic-reuse/02-AppPagination/components/AppPagination.vue
+++ 08-interactions-and-logic-reuse/02-AppPagination/components/AppPagination.vue
@@ -1,12 +1,34 @@
 <template>
   <div>
-    <!-- Решение -->
+    <template v-for="item in currentItems">
+      <slot :item="item" />
+    </template>
   </div>
 </template>
 
 <script>
 export default {
   name: 'AppPagination',
+  props: {
+    page: {
+      type: Number,
+    },
+    perPage: {
+      type: Number,
+    },
+    items: {
+      type: Array,
+    },
+  },
+  computed: {
+    currentItems() {
+      let index = this.page * this.perPage - this.perPage;
+      let count = this.perPage;
+      return Array.isArray(this.items)
+        ? [...this.items].splice(index, count)
+        : [];
+    },
+  },
 };
 </script>
 
